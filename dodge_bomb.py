55678def8948dfc6ffa7f52991a0931344a61280
Ayumu Tadokoro <c0b2209528@edu.teu.ac.jp>
2023-11-28 17:44:24 +0900

    コメントの追加 #1

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -24,6 +24,7 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    # こうかとんを矢印キーによって切り替えるための辞書の作成
     kk_img_dct = {(-5, 0):pg.transform.rotozoom(kk_img, 0, 1), (-5, 5):pg.transform.rotozoom(kk_img, 45, 1), (0, 5):pg.transform.rotozoom(kk_img, 90, 1), (5, 5):pg.transform.rotozoom(kk_img, 135, 1), (5, 0):pg.transform.rotozoom(kk_img, 180, 1), (5, -5):pg.transform.rotozoom(kk_img, 225, 1), (0, -5):pg.transform.rotozoom(kk_img, 270, 1), (-5, -5):pg.transform.rotozoom(kk_img, 315, 1)}
     kk_img_rct = kk_img.get_rect()
     kk_img_rct.center = 900, 400
@@ -31,13 +32,14 @@ def main():
     enn1 = pg.Surface((20, 20))
     enn.set_colorkey((0, 0, 0))
     enn1.set_colorkey((0, 0, 0))
-    pg.draw.circle(enn, (255, 0, 0), (10, 10), 10) 
-    pg.draw.circle(enn1, (0, 255, 0), (10, 10), 10)
+    pg.draw.circle(enn, (255, 0, 0), (10, 10), 10) # 半径10の赤い円を作成
+    pg.draw.circle(enn1, (0, 255, 0), (10, 10), 10) # 半径10の青い円を作成
     enn_rct = enn.get_rect()
     enn_rct.center = WIDTH/2, HEIGHT/2
     enn1_rct = enn1.get_rect()
     enn1_rct.center = WIDTH/2, HEIGHT/2
-    vx, vy = 5, 5
+    # vx:横方向速度, vy:縦方向速度
+    vx, vy = 5, 5 
     vx1, vy1 = 10, 10
     clock = pg.time.Clock()
     tmr = 0
@@ -45,23 +47,26 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
+        # 衝突判定
         if kk_img_rct.colliderect(enn_rct):
             print("Game Over")
             return
 
-        sum_move = [0, 0]
+        sum_move = [0, 0] # 合計移動量の初期化 
         key_lst = pg.key.get_pressed()
-        if key_lst[pg.K_UP]: sum_move[1] -= 5
-        if key_lst[pg.K_DOWN]: sum_move[1] += 5
-        if key_lst[pg.K_LEFT]: sum_move[0] -= 5
-        if key_lst[pg.K_RIGHT]: sum_move[0] += 5
+        if key_lst[pg.K_UP]: sum_move[1] -= 5 # 上矢印
+        if key_lst[pg.K_DOWN]: sum_move[1] += 5 # 下矢印
+        if key_lst[pg.K_LEFT]: sum_move[0] -= 5 # 左矢印
+        if key_lst[pg.K_RIGHT]: sum_move[0] += 5 # 右矢印
         
         screen.blit(bg_img, [0, 0])
         kk_img_rct.move_ip(sum_move[0], sum_move[1]) 
         if screen_judg(kk_img_rct) != (True, True):
-            kk_img_rct.move_ip(-sum_move[0], -sum_move[1])
+            kk_img_rct.move_ip(-sum_move[0], -sum_move[1]) # 速度に応じて位置を移動させる
+        # 動いていない場合
         if sum_move == [0, 0]:
             screen.blit(kk_img, kk_img_rct)
+        # 動いている場合
         else:
             screen.blit(kk_img_dct[tuple(sum_move)], kk_img_rct)
             kk_img = kk_img_dct[tuple(sum_move)]
@@ -72,6 +77,7 @@ def main():
             vy *= -1
         enn_rct.move_ip(vx, vy)
         screen.blit(enn, enn_rct)
+        # tmpが500を超えた時に爆弾が1つ増える
         if tmr > 50*10:
             width, height = screen_judg(enn1_rct)
             if not width:
